eb1a50631015727b5c03cb51ac633642e2cdbeea
hod284 <61046278+hod284@users.noreply.github.com>
2025-12-16 18:02:44 +0900

    QQE22
    32132321

---
+++ src/main/java/com/example/gRPCBackend/DatabaseInitiallzer.java
@@ -0,0 +1,31 @@
+package com.example.gRPCBackend;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class DatabaseInitiallzer {
+
+    private JdbcTemplate jdbcTemplate;
+    public DatabaseInitiallzer(JdbcTemplate jTemplate) {
+        jdbcTemplate = jTemplate;
+    }
+    @PostConstruct
+    private void createPlateRecordsTable() {
+        String sql = """
+            CREATE TABLE IF NOT EXISTS carinformation (
+                id SERIAL PRIMARY KEY,
+                plate_number VARCHAR(20) NOT NULL,
+                bad_point BIGINT,
+                driver_owner VARCHAR(255)
+            )
+        """;
+        
+        jdbcTemplate.execute(sql);
+        System.out.println("✅ plate_records 테이블 준비 완료");
+    }
+
+
+}
